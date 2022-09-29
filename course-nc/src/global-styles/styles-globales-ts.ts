1271120cb336166902ecb5189769412dbdfce29f
Andres Giraldo Arenas <andresganc@gmail.com>
2022-09-28 19:44:29 -0500

    Structure & Files

---
+++ course-nc/src/global-styles/styles-globales-ts.ts
@@ -0,0 +1,11 @@
+
+import { createGlobalStyle } from 'styled-components'
+
+const stylesGlobalesTS = createGlobalStyle `
+    * {
+        margin: 0;
+        padding: 0;
+    }
+`
+
+export default stylesGlobalesTS
